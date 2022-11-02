3f8a7564e90748106a9d7889454eb405f28f7d75
edupoll <101759496+edupoll@users.noreply.github.com>
2022-11-02 13:16:16 +0900

    초기 프로젝트 설정

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>여행은 살아보는 거야</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
