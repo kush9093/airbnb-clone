cdd7b14b0801eafdc94e8410172aaf5561196a9a
kush9093 <0bbd09@gmail.com>
2022-11-18 16:02:57 +0900

    update: three test

--- pages/api/hosting/uploadPhotos.ts
+++ pages/api/hosting/uploadPhotos.ts
@@ -1,6 +1,24 @@
 import formidable from "formidable";
 import { NextApiRequest, NextApiResponse, NextConfig } from "next";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getDownloadURL, getStorage,ref, uploadBytes} from "firebase/storage"
+import fs from "fs"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC17qxGa463wVKthjp2iEKKMm8Gh1llXLI",
+  authDomain: "clone-project-a674d.firebaseapp.com",
+  projectId: "clone-project-a674d",
+  storageBucket: "clone-project-a674d.appspot.com",
+  messagingSenderId: "1085707833456",
+  appId: "1:1085707833456:web:3d415e2ecd73b6601064b5"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
 
 export const config:NextConfig = {
     api:{
@@ -17,12 +35,39 @@ export default async function handler(
     console.log("====uploadPhtos ====");
     console.log("req.body",req.body)
     const form = formidable({multiples:true});
-    form.parse(req,(err,fields,files)=>{
-        if(err){
-           return console.log("!!!! ERROR !!!",err)
-        }
-        console.log(fields);
-        console.log(files);
-    })
+
+    console.log("=== uploadPhotos ===");
+
+    const result = await new Promise((resolve,reject)=>{
+        form.parse(req,(err,fields,files)=>{
+            resolve({
+                itemId: fields.itemId as string,
+                photos: files.photos as formidable.File[],
+            });
+        });
+    });
+    console.log(result);
+
+
+    // form.parse(req,async (err,fields,files)=>{
+    //     if(err){
+    //        return console.log("!!!! ERROR !!!",err)
+    //     }
+    //     console.log("fields",fields);
+    //     // console.log(files);
+
+    //     const storage = getStorage(firebaseApp);
+
+    //     const dirRef = ref(storage,"hosting/",fields.itemId)
+    //     for(let one of files.photos as formidable.File[]){
+    //         const fileRef = ref(dirRef, one.newFilename);
+    //         const file = fs.readFileSync(one.filepath);
+    //         console.log(file);
+    //         const result = await uploadBytes(fileRef,file,{contentType:one.mimetype!});
+    //         console.log(result);
+    //         const url = await getDownloadURL(fileRef);
+    //         console.log(url);
+    //     }
+    // })
     return res.status(200).json({})
 }
--- pages/become-a-host/[itemId]/photos.tsx
+++ pages/become-a-host/[itemId]/photos.tsx
@@ -10,40 +10,52 @@ import ImageUploadBox from "../../../components/photo/ImageUploadBox";
 
 
 
-export default function PhotosPage(prop:any) {
-    const {itemId} = prop;
+export default function PhotosPage(prop: any) {
+    const { itemId } = prop;
     const [btn, setBtn] = React.useState(false);
     const router = useRouter();
-    const [ImageList,setImageList] = React.useState([]);
+    const [ImageList, setImageList] = React.useState([]);
     const nextStepHandle = async () => {
         //업데이트 부분
         const formData = new FormData();
 
-        formData.append("itemId",itemId as string);
+        formData.append("itemId", itemId as string);
 
-        ImageList.forEach((one)=>{
-            formData.append("photos",one);
+
+        function dataURLtoFile(dataurl, filename) {
+            var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+                bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+            while (n--) {
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+            return new File([u8arr], filename, { type: mime });
+        }
+
+        ImageList.forEach((one) => {
+            const file = dataURLtoFile(one, Date.now()+Math.floor(Math.random()*100));
+            console.log(file);
+            formData.append("photos", file);
         })
 
 
-        const response = fetch("/api/hosting/uploadPhotos",{
-            method:"POST",
-            body:formData,
+        const response = fetch("/api/hosting/uploadPhotos", {
+            method: "POST",
+            body: formData,
         })
 
 
         // router.push("/become-a-host/" + itemId + "/title");
     };
 
-    const ImageHandle = (arr) =>{
+    const ImageHandle = (arr) => {
         setImageList(arr);
     }
 
-    const previousHandle = () =>{
+    const previousHandle = () => {
         router.push({
             pathname: '/become-a-host/[itemId]/location',
             query: { itemId },
-          })
+        })
     }
     return (
         <>
@@ -51,14 +63,14 @@ export default function PhotosPage(prop:any) {
                 <Grid container spacing={2} columns={16}>
                     <Grid sx={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", background: "linear-gradient(to top, #3B07BB, 50%, #E61E4D)" }} item xs={8}>
                         <Box>
-                            <Typography sx={{ color: "white",width:"100%",textAlign:"center" }} variant="h3" gutterBottom>
+                            <Typography sx={{ color: "white", width: "100%", textAlign: "center" }} variant="h3" gutterBottom>
                                 <b>게스트가 머무르게 될 숙소의 종류가 무엇인가요?</b>
                             </Typography>
                         </Box>
                     </Grid>
                     <Grid item xs={8} sx={{ backgroundColor: "white", height: "100vh" }}>
                         <Box sx={{ display: "flex", justifyContent: "space-between", flexDirection: "column", height: "100vh" }}>
-                            <Box sx={{ mt:3,mx: 4, display: "flex", justifyContent: "flex-end" }}>
+                            <Box sx={{ mt: 3, mx: 4, display: "flex", justifyContent: "flex-end" }}>
                                 <Box sx={{ display: "flex", gap: 1 }}>
                                     <Link href="/become-a-host/property-type-group">
                                         <Chip variant="outlined" avatar={<Avatar>M</Avatar>} label="슈퍼호스트에게 물어보기" />
@@ -68,13 +80,13 @@ export default function PhotosPage(prop:any) {
                                 </Box>
                             </Box>
                             <Box>
-                                    <ImageUploadBox ImageHandle={ImageHandle} />
+                                <ImageUploadBox ImageHandle={ImageHandle} />
 
                             </Box>
                             <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                                    <Button onClick={previousHandle} sx={{ mr: 5, mb: 2, px: 3.5, py: 1, backgroundColor: "#E61E4D" }} variant="contained" color="error">
-                                        <b>뒤로</b>
-                                    </Button>
+                                <Button onClick={previousHandle} sx={{ mr: 5, mb: 2, px: 3.5, py: 1, backgroundColor: "#E61E4D" }} variant="contained" color="error">
+                                    <b>뒤로</b>
+                                </Button>
                                 <Button onClick={nextStepHandle} disabled={btn} sx={{ mr: 5, mb: 2, px: 3.5, py: 1, backgroundColor: "#E61E4D" }} variant="contained" color="error">
                                     <b>다음</b>
                                 </Button>
@@ -90,10 +102,10 @@ export default function PhotosPage(prop:any) {
 }
 
 
-export const getServerSideProps:GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
 
     return {
-        props:{itemId:context.params?.itemId!},
+        props: { itemId: context.params?.itemId! },
     };
 
 
